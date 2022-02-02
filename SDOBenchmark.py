b8c1508c4b986bb03d983d102f1218ed65a7471a
yashtyk <yana.shtyk@fhnw.ch>
2022-02-02 14:57:07 +0200

    files with code

---
+++ SDOBenchmark.py
@@ -0,0 +1,255 @@
+import datetime as dt
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Callable, Optional, List, Union, Sequence
+
+from PIL import Image
+import pandas as pd
+import numpy as np
+import copy
+
+import random
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+import torchvision.transforms.functional as F
+
+
+class BaseDataset (Dataset, ABC):
+    @abstractmethod
+    def y(self, indices: Optional[Sequence[int]] = None) -> List:
+        pass
+
+
+
+class SDOBenchmarkDataset_time_steps(BaseDataset):
+    def __init__(
+        self,
+        csv_file: Path,
+        root_folder: Path,
+        channel="171",
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        time_steps: Union[int, List[int]] = 0,
+        non_flare_only = True,
+        ls = None,
+        is_train = True
+    ):
+        metadata = pd.read_csv(csv_file, parse_dates=["start", "end"])
+        self.is_train = is_train
+        self.non_flare_only = non_flare_only
+        self.treshold = 1e-6
+        self.root_folder = root_folder
+        self.channel = channel
+        self.transform = transform
+        self.target_transform = target_transform
+
+        self.time_steps_values = [0, 7 * 60, 10 * 60 + 30, 11 * 60 + 50]
+        self.time_steps = time_steps if isinstance(time_steps, list) else [time_steps]
+
+
+        if ls is None:
+            self.setup(metadata)
+        else:
+            self.ls = ls
+
+    def setup(self, metadata):
+        ls = []
+        to_tensor  = transforms.ToTensor()
+        for i in range(len(metadata)):
+            is_step=[]
+            sample_metadata = metadata.iloc[i]
+            target = sample_metadata["peak_flux"]
+            if self.non_flare_only and target > self.treshold:
+                continue
+            if self.target_transform is not None and \
+                isinstance(self.target_transform(target), int) and \
+                self.target_transform(target) < 0:
+                # Ignore sample if it is not part of a class
+                continue
+
+            sample_active_region, sample_date = sample_metadata["id"].split("_", maxsplit=1)
+
+            paths: List[Path] = []
+            for time_step in self.time_steps:
+                image_date = sample_metadata["start"] + dt.timedelta(minutes=self.time_steps_values[time_step])
+                image_date_str = dt.datetime.strftime(image_date, "%Y-%m-%dT%H%M%S")
+                image_name = f"{image_date_str}__{self.channel}.jpg"
+                paths.append(Path(sample_active_region) / sample_date / image_name)
+
+
+
+            if not all((self.root_folder / path).exists() for path in paths):
+                paths1 = [path for path in paths if (self.root_folder / path).exists()]
+                is_step = [idx for idx, path in enumerate(paths) if not (self.root_folder / path).exists()]
+                if len(paths1) < 3:
+                    continue
+                else:
+                    paths = paths1
+
+            # check and delete black images
+            for idx, path in enumerate(paths):
+                image = Image.open(self.root_folder / path)
+                image = to_tensor(image)
+                if image.std() == 0:
+                    paths.remove(path)
+                    is_step.append(idx)
+
+            # check if 3 time steps exist
+
+            if len(paths) < 3:
+                continue
+
+            if len(paths) == 3:
+                if is_step[0]<3:
+                    num_replace = is_step[0]
+                else:
+                    num_replace = 2
+
+                img_replace = copy.deepcopy(paths[num_replace])
+
+                paths.insert(is_step[0], img_replace)
+
+
+
+
+
+            ls.append((paths, target))
+
+        self.ls = ls
+    def transform_byol(self, x):
+        p = random.random()
+        if p <= 0.3:
+
+            brightness = 0.8
+            hue = 0.2
+            chosen_brightness = random.uniform(max(0, 1-brightness), 1+brightness)
+            chosen_hue = random.uniform(-hue, hue)
+
+            x_t = [F.adjust_brightness(x[i], chosen_brightness) for i in range ( 4)]
+            x = x_t
+            x_t = [F.adjust_hue(x[i], chosen_hue) for i in range (4)]
+            x= x_t
+
+        p = random.random()
+
+        if p <= 0.5:
+            x_t = [F.hflip(x[i]) for i in range( 4)]
+            x= x_t
+
+        p = random.random()
+
+        #if p <= 0.2:
+            #kernel_size =(3, 3)
+            #sigma= random.uniform(1.0, 2.0)
+            #x_t = [F.gaussian_blur(x[i], kernel_size, [sigma, sigma]) for i in range (4)]
+            #x= x_t
+        #random resized crop
+        size = 256
+        i, j, h, w = transforms.RandomResizedCrop.get_params(x[0], (0.8, 1), (1, 1))
+        x_t = [F.resized_crop(x[m], i, j, h, w, size=(size, size) ) for m in range(4)]
+        x= x_t
+
+        return x
+
+
+
+
+
+
+
+
+
+    def __len__(self) -> int:
+        return len(self.ls)
+
+    def __getitem__(self, index):
+        metadata = self.ls[index]
+        target = metadata[1]
+        images = [Image.open(self.root_folder / path) for path in metadata[0]]
+
+        if self.is_train:
+            image1 = self.transform_byol(images)
+            image2 = self.transform_byol(images)
+
+        to_tensor = transforms.ToTensor()
+        images1 = [to_tensor(image) for image in images]
+        images = images1
+
+        image11 = [to_tensor(image) for image in image1]
+        image1 = image11
+
+        image22 = [to_tensor(image) for image in image2]
+        image2 = image22
+
+        if self.transform:
+            images = [self.transform(image) for image in images]
+            image1 = [self.transform(image) for image in image1]
+            image2 = [self.transform(image) for image in image2]
+
+        if self.target_transform:
+            target1 = self.target_transform(target)
+
+        if not torch.is_tensor(images[0]):
+            return images[0], target
+
+        # Put images of different time steps as one image of multiple channels (time steps ~ rgb)
+        image = torch.cat(images, 0)
+        images1 = torch.cat(image1, 0)
+        images2 = torch.cat(image2, 0)
+
+        return image, images1, images2, target1
+
+    def y(self, indices: Optional[Sequence[int]] = None) -> list:
+        ls = self.ls
+        if indices is not None:
+            ls = (self.ls[i] for i in indices)
+
+        #if self.target_transform is not None:
+        #    return [self.target_transform(y[1]) for y in ls]
+
+        return [y[1] for y in ls]
+
+
+class SDOBenchmarkDataset_time_step_pairs(SDOBenchmarkDataset_time_steps):
+    def __init__(
+            self,
+            csv_file: Path,
+            root_folder: Path,
+            channel="171",
+            transform: Optional[Callable] = None,
+            target_transform: Optional[Callable] = None,
+            time_steps: Union[int, List[int]] = 0,
+            indx: List[int] = [0],
+            non_flare_only = False,
+            ls= None
+
+    ):
+        super().__init__( csv_file, root_folder, channel,transform,target_transform,time_steps, ls =ls)
+        self.indx = indx
+        self.non_flare_only = non_flare_only
+        self.make_pairs()
+    def make_pairs(self):
+        idx = self.indx.copy()
+        np.random.shuffle(idx)
+        pairs = [[idx[2*i], idx[2*i+1]] for i in range(0, len(idx)//2)]
+        self.pairs = pairs
+
+        print(pairs[0])
+
+    def __len__(self) -> int:
+        return len(self.pairs)
+
+    def __getitem__(self, index):
+        ind1 = self.pairs[index][0]
+        ind2 = self.pairs[index][1]
+        img1, target, _ =SDOBenchmarkDataset_time_steps.__getitem__(self, ind1)
+        img2, _, _ = SDOBenchmarkDataset_time_steps.__getitem__(self, ind2)
+
+        return img1, img2, target
+
+
+
+
+
+
---
+++ train.py
@@ -0,0 +1,166 @@
+import argparse
+import os
+from pathlib import Path
+import torch
+from torchvision import models
+from byol import BYOL
+import torch.nn as nn
+import yaml
+
+
+import math
+
+from utils import flux_to_class_builder
+from SDOBenchmark import SDOBenchmarkDataset_time_steps
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+parser = argparse.ArgumentParser(description="...")
+
+parser.add_argument("--config_path", default="./config.yaml", type=str, help="The config file path")
+parser.add_argument("--out_file", default = "./out.txt", type=str, help = "Output file")
+parser.add_argument("--save_weights", default ="./weights", type = str, help = "Path to folder where model weights will be saved")
+parser.add_argument('--alpha', default = 1, type = float, help = 'weight of classification loss')
+parser.add_argument('--epoch_pretr', default = -1, type = int, help = 'number of pretrained model')
+parser.add_argument('--pretr_path', default = './weights/model.pth', type = str, help = 'path to pretrained weights' )
+
+args = parser.parse_args()
+
+#device
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+def toDevice(model):
+    return model.to(device)
+
+def initLearner():
+    resnet = models.resnet18()
+    #change the number of input channels to 4
+    resnet.conv1 = nn.Conv2d(4, 64, kernel_size=(7, 7), stride = (2, 2), padding=(3, 3), bias=False)
+    learner = BYOL(
+        resnet,
+        image_size=256,
+        hidden_layer='avgpool',
+        num_channel=4
+    )
+    learner = toDevice(learner)
+
+    return learner
+
+def train_test_split(dataset, test_size: float = 0.1,):
+    if test_size < 0 or test_size > 1:
+        raise ValueError("Test size must be in [0, 1].")
+
+    test_size = int(test_size * len(dataset))
+    train_size = len(dataset) - test_size
+
+    generator = torch.Generator()
+
+    return torch.utils.data.random_split(dataset, [train_size, test_size], generator=generator)
+
+def run(parameters):
+
+    channel = parameters ["data"]["channel"]
+
+    if not os.path.isdir(args.save_weights):
+        os.mkdir(args.save_weights)
+
+    # model initialisation
+
+    learner = initLearner()
+    print(learner)
+
+    lr = 0.005
+    opt = torch.optim.Adam(learner.parameters(), lr=lr)
+    scheduler = CosineAnnealingLR(opt, 300, 0.0001)
+    # dataset initialisation
+
+    path = Path (parameters["data"]["path"])
+    size = parameters["data"]["size"]
+    target_transform = flux_to_class_builder(parameters["data"]["targets"]["classes"])
+    time_steps = parameters["data"]["time_steps"]
+
+    dataset = SDOBenchmarkDataset_time_steps(
+        path / 'training' / 'meta_data.csv',
+        path / 'training',
+        target_transform = target_transform,
+        channel = channel,
+        time_steps = time_steps,
+        non_flare_only= False,
+    )
+
+    dataset_tr, dataset_val = train_test_split(dataset)
+
+    dataloader_tr = torch.utils.data.DataLoader(dataset_tr, batch_size=200, shuffle=True)
+    dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=200, shuffle=True)
+
+    learner.target_ema_updater.beta = 0.996
+
+    epochs = 300
+    alpha = args.alpha
+
+    f = open(Path(args.out_file), 'a')
+    f.write('training on {}, lr: {}, weights_saved: {} , alpha: {}\n'.format(parameters["data"]["channel"], lr, args.save_weights, args.alpha))
+    f.close()
+
+    # create directory to save weights
+    if not os.path.isdir(Path(args.save_weights) ):
+        os.mkdir(Path(args.save_weights))
+
+    #training
+    # load weights
+    if args.epoch_pretr >=0:
+        learner.load_state_dict(torch.load(args.pretr_path))
+
+        for epoch in range (0, args.epoch_pretr+1):
+            scheduler.step()
+
+    start = (args.epoch_pretr + 1) if args.epoch_pretr >=0 else 0
+    for epoch in range(start,  epochs):
+        learner.train()
+        learner.target_ema_updater.beta = float(1 - (1 - 0.996) * (math.cos(math.pi * epoch / 300) + 1) / 2)
+        loss_tr = 0
+        count = 0
+        print(epoch)
+        for img, img1, img2, label in dataloader_tr:
+            img, img1, img2, label= img.to(device), img1.to(device), img2.to(device), label.to(device)
+
+            loss, loss_cl = learner( img,img1, img2,  label)
+            loss_all = loss + alpha* loss_cl
+            opt.zero_grad()
+            loss_all.backward()
+            opt.step()
+            loss_tr += loss_all.detach()
+            count += 1
+            learner.update_moving_average()  # update moving average of target encoder
+
+        torch.save(learner.state_dict(), Path(args.save_weights) / 'model_{}.pth'.format(epoch))
+        scheduler.step()
+        # validation
+        with torch.no_grad():
+            loss_vall = 0
+            count_vall = 0
+            learner.eval()
+            for img, img1, img2, label in dataloader_val:
+                img, img1, img2, label= img.to(device), img1.to(device), img2.to(device), label.to(device)
+
+                loss, loss_cl = learner( img,img1, img2,  label)
+
+
+                loss_all = loss + alpha * loss_cl
+                loss_vall += loss_all.detach()
+                count_vall+=1
+            f = open(Path(args.out_file), 'a')
+            f.write('epoch: {} , loss: {:.4f}, loss_val : {:4f}, lr: {}\n'.format(epoch, loss_tr / count,
+                                                                                  loss_vall / count_vall,
+                                                                                  opt.param_groups[0]['lr']))
+            f.close()
+
+
+if __name__ == '__main__':
+    # --- configs and constants ----------------------------------------------------------------------------------------
+    with open(args.config_path) as file:
+        parameters = yaml.load(file, Loader=yaml.FullLoader)
+
+    run(parameters)
+
+
